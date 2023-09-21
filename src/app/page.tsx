71d5ef52845b5116720ec50acae36fb58302dddc
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-09-21 19:43:04 -0300

    feat: portifolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,20 +33,20 @@ export default function Home() {
           className='rounded-full h-62 w-62 max-lg:h-36 max-sm:w-36 opacity-0 scale-0 animate-surgir'
         ></Image>
       </div>
-      <div className='bg-white bg-opacity-50 shadow-lg rounded-md flex flex-col flex-1 items-center justify-center p-40 max-sm:p-1'>
+      <div className='bg-white bg-opacity-20 shadow-lg rounded-md flex flex-col flex-1 items-center justify-center p-40 max-sm:p-1'>
         <div className='p-2 max-sm:p-0 flex flex-1 flex-col items-center justify-center translate-x-full animate-surgir-direita'>
-          <h4 className='text-marinho text-md font-bold max-sm:hidden'>
+          <h4 className='text-white text-md font-bold max-sm:hidden'>
             Desenvovedor web full stack
           </h4>
-          <p className='text-justify mt-4 max-sm:mt-1 '>
+          <p className='text-white text-justify mt-4 max-sm:mt-1 '>
             Sou Desenvolvedor Web, com formação em ciências da computação,
             experiência com testes de funcionalidade, gerenciamento de equipes e
             apaixonado por criar aplicações que impactem a vida das pessoas.
           </p>
         </div>
 
-        <h2 className='font-bold mt-4 max-sm:mt-1'>Stacks</h2>
-        <ul className='flex flex-wrap items-center justify-center mt-4 max-sm:mt-1 opacity-0 -translate-x-full animate-surgir-esquerda'>
+        <h2 className='text-white font-bold mt-4 max-sm:mt-1'>Stacks</h2>
+        <ul className='flex flex-wrap items-center justify-center mt-4 max-sm:mt-1 opacity-0 translate-x-full animate-surgir-esquerda'>
           {stacks.map((stack) => (
             <li
               key={stack}
@@ -60,7 +60,7 @@ export default function Home() {
         <a
           href='/RonanFernandoSalvador.pdf'
           target='_blank'
-          rel='noopener noreferrer'
+          // rel='noopener noreferrer'
           className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 max-sm:mt-1'
         >
           Visualizar CV
--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -7,7 +7,7 @@ function page() {
   return (
     <div className='h-5/6 w-5/6 flex flex-col items-center max-sm:h-full max-sm:w-full'>
       <h1 className='text-2xl font-bold mb-4'>Meus Projetos</h1>
-      <div className='flex items-center justify-evenly border-solid border-2 border-sky-500 w-2/3 p-1 rounded-lg max-sm:w-full bg-sky-500'>
+      <div className='flex items-center justify-evenly w-2/3 p-1 rounded-lg max-sm:w-full bg-white bg-opacity-50'>
         <button className='bg-gray-900 p-2 text-white rounded-lg hover:scale-125 max-sm:text-xs'>
           Todos
         </button>
@@ -25,7 +25,7 @@ function page() {
         {frontProjects.map((project) => (
           <div
             key={project.id}
-            className='w-1/4 max-sm:w-full h-40 bg-white shadow-lg  rounded-lg m-3'
+            className='w-1/4 max-sm:w-full h-40 bg-white bg-opacity-50 shadow-lg  rounded-lg m-3 translate-x-full animate-surgir'
           >
             <CardProject project={project} />
           </div>
