5f82882e50e6a4981d30b381dbc0bf94b745babf
Ronan Salvador <ronansalvador@yahoo.com.br>
2024-07-19 11:19:07 -0300

    feat(new route): constelacao

--- src/app/constelacao/constelacaofamiliar/page.tsx
+++ src/app/constelacao/constelacaofamiliar/page.tsx
@@ -1,5 +1,5 @@
 import Atendimento from '../components/Atendimento'
-import Button from '../components/Button'
+import Button from '../../components/Button'
 import Title from '../components/Title'
 
 function ConstelacaoFamiliar() {
