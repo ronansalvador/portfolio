92dd8456b3114266508820a13be56985f2a8113b
Ronan Salvador <ronansalvador@yahoo.com.br>
2024-07-21 10:13:09 -0300

    style: atualziando responsividade portfolio

--- src/app/(portfolio)/page.tsx
+++ src/app/(portfolio)/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+
 const stacks = [
   'JavaScript',
   'TypeScript',
@@ -22,22 +23,22 @@ const stacks = [
 
 export default function Home() {
   return (
-    <div className="h-5/6 w-5/6 flex max-xl:flex-col max-sm:h-full max-sm:w-full">
-      <div className="flex items-center justify-center p-8 max-md:p-1">
+    <div className="h-5/6 w-5/6 flex flex-col lg:flex-row max-sm:h-full max-sm:w-full">
+      <div className="flex items-center justify-center p-8 md:p-4 max-md:p-2">
         <Image
           src="/ronan.jpeg"
-          width="300"
-          height="300"
+          width={300}
+          height={300}
           alt="Ronan Salvador"
-          className="rounded-full h-62 w-62 max-sm:h-36 max-sm:w-36 opacity-0 scale-0 animate-surgir"
-        ></Image>
+          className="rounded-full h-62 w-62 max-sm:h-36 max-sm:w-36 md:h-48 md:w-48 lg:h-60 lg:w-60 opacity-0 scale-0 animate-surgir"
+        />
       </div>
-      <div className="bg-white bg-opacity-20 shadow-lg rounded-md flex flex-col flex-1 items-center justify-center max-sm:p-1">
+      <div className="bg-white bg-opacity-20 shadow-lg rounded-md flex flex-col flex-1 items-center justify-center p-8 md:p-4 max-sm:p-2">
         <div className="w-3/4 mx-auto flex flex-col items-center justify-center translate-x-full animate-surgir-direita">
           <h4 className="text-white text-md font-bold max-sm:hidden">
-            Desenvovedor web full stack
+            Desenvolvedor web full stack
           </h4>
-          <p className="text-white text-justify mt-4 max-sm:mt-1 ">
+          <p className="text-white text-justify mt-4 max-sm:mt-1 text-sm md:text-base lg:text-lg">
             Sou Desenvolvedor Web Full Stack com formação em Ciências da
             Computação, possuo vasta experiência em testes e gerenciamento de
             equipes. Sou apaixonado por criar aplicações que impactam
@@ -47,12 +48,14 @@ export default function Home() {
           </p>
         </div>
 
-        <h2 className="text-white font-bold mt-4 max-sm:mt-1">Stacks</h2>
-        <ul className=" w-3/4 mx-auto flex flex-wrap items-center justify-center mt-4 max-sm:mt-1 opacity-0 translate-x-full animate-surgir-esquerda max-sm:w-11/12">
+        <h2 className="text-white font-bold mt-4 max-sm:mt-1 text-lg md:text-xl lg:text-2xl">
+          Stacks
+        </h2>
+        <ul className="w-3/4 mx-auto flex flex-wrap items-center justify-center mt-4 max-sm:mt-1 opacity-0 translate-x-full animate-surgir-esquerda max-sm:w-11/12">
           {stacks.map((stack) => (
             <li
               key={stack}
-              className="bg-verde text-white m-0.5 rounded p-2 max-sm:p-1"
+              className="bg-verde text-white m-0.5 rounded p-2 max-sm:p-1 text-xs md:text-sm lg:text-base"
             >
               {stack}
             </li>
